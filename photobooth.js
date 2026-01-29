e7823e3fe00ab35d4d9a043e12fb1b3005573e1e
Sachin Sandhu <sachinsandhu@Sachins-MacBook-Pro.local>
2026-01-28 16:28:45 -0800

    Update site

--- photobooth.js
+++ photobooth.js
@@ -71,6 +71,7 @@ if (stopBtn) stopBtn.addEventListener('click', stopCamera);
 if (singlePhotoBtn) singlePhotoBtn.addEventListener('click', takePhoto);
 if (photoStripBtn) photoStripBtn.addEventListener('click', takePhotoStrip);
 if (photo4x6Btn) photo4x6Btn.addEventListener('click', take4x6Photo);
+if (collageBtn) collageBtn.addEventListener('click', take4x6Collage);
 if (videoRecordBtn) videoRecordBtn.addEventListener('click', toggleVideoRecording);
 
 // Fullscreen booth handlers
